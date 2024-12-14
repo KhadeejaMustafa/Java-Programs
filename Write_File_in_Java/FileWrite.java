956563e880f919be4dfad3685b646503e4d59770
Kdeeja_Mstafa <118957732+KhadeejaMustafa@users.noreply.github.com>
2024-12-14 21:14:21 +0300

    Update FileWrite.java

--- Write_File_in_Java/FileWrite.java
+++ Write_File_in_Java/FileWrite.java
@@ -8,11 +8,11 @@ public class FileWrite {
         FileWriter fwriter = null;
         PrintWriter pwriter = null;
       try{
-          fwriter = new FileWriter("story.txt");
+          fwriter = new FileWriter("rosefacts.txt");
           pwriter = new PrintWriter(fwriter);
           
-          String S1 = "";
-          String S2 = "";
+          String S1 = "Some facts about Roses:";
+          String S2 = "\033[1mRoses Have Been Around For Over 35 Million Years.\033[0m \nFossil evidence shows that roses have been present on Earth for over 35 million years. \nThis ancient lineage indicates that roses have successfully evolved and adapted to a variety of climates and landscapes. \nTheir resilience through time highlights their deep-rooted place in nature’s history.";
           
           pwriter.println(S1);
           pwriter.println(S2);
